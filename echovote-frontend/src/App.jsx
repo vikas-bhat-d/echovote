4804ac6ad3a80069d4aaa40490a9bc87b71b923e
Dhanaush12212 <dhanush12212@gmail.com>
2024-11-05 21:47:08 +0530

    Feature: Completed Landing Page

--- echovote-frontend/src/App.jsx
+++ echovote-frontend/src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import LandingPage from './Components/LandingPage'
 
 function App() {
   return (
-    <div>App</div>
+    <div>
+      <LandingPage/>
+    </div>
   )
 }
 
