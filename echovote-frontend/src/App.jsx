d9804051eb66d38dec3513faf7855acc905ab559
Dhanaush12212 <dhanush12212@gmail.com>
2024-11-05 23:46:34 +0530

    Inprogress coding Login Page

--- echovote-frontend/src/App.jsx
+++ echovote-frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import LandingPage from './Components/LandingPage'
+import LoginPage from './Components/LoginPage'
 
 function App() {
   return (
     <div>
       <LandingPage/>
+      <LoginPage/>
     </div>
   )
 }
